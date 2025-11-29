b0c46c6af6f8b9b4123e7a1a336a7a8302240f1b
dwlong1896 <long.doquang1896@hcmut.edu.vn>
2025-11-29 23:07:01 +0700

    add frontend feedback

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,40 @@
-import './App.css';
-//import RegisterTutor from './RegisterTutor';
-//import Layout from './Layout';
-import TutorFreeSlot from './TutorFreeSlot'; 
-import AppointmentBooking from './AppointmentBooking';
-
+import "./App.css";
+// import RegisterTutor from './RegisterTutor'; // Cái nào không dùng thì comment
+import LayoutTutor from "./layout/layoutTutor/Layout"; // <--- QUAN TRỌNG: Bỏ comment dòng này
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import AppointmentBooking from './AppointmentBooking';
+import LayoutStudent from "./layout/layoutSv/LayoutSv";
+import CourseStd from "./pages/feedback/CourseStd";
+import FeedbackPage from "./pages/feedback/FeedbackPage";
 function App() {
   return (
-    //<Layout>
-      //<TutorFreeSlot/>
-      <AppointmentBooking studentId={1} tutorId={1} />
-    //</Layout>
+    // <LayoutTutor>
+    //   <TutorFreeSlot />
+    // </LayoutTutor>
+    // <LayoutStudent>
+    //   <CourseStd></CourseStd>
+    // </LayoutStudent>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/course"
+          element={
+            <LayoutStudent>
+              <CourseStd />
+            </LayoutStudent>
+          }
+        />
+
+        <Route
+          path="/course/feedback/:id"
+          element={
+            <LayoutStudent>
+              <FeedbackPage />
+            </LayoutStudent>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
